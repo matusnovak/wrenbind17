e676653d2f7832456cb2009e68bc3934711afc2e
Matus Novak <email@matusnovak.com>
2020-07-15 21:28:16 +0200

    Fix catch2 include dir

--- modules/FindCatch2.cmake
+++ modules/FindCatch2.cmake
@@ -1,7 +1,7 @@
 include(FindPackageHandleStandardArgs)
 
 if(NOT TARGET Catch2)
-  find_path(CATCH2_INCLUDE_DIR NAMES catch.hpp PATHS ${CMAKE_CURRENT_LIST_DIR}/../libs/Catch2/include)
+  find_path(CATCH2_INCLUDE_DIR NAMES catch2/catch.hpp PATHS ${CMAKE_CURRENT_LIST_DIR}/../libs/Catch2/single_include)
   mark_as_advanced(FORCE CATCH2_INCLUDE_DIR)
   add_library(Catch2 INTERFACE IMPORTED)
   set_target_properties(Catch2 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CATCH2_INCLUDE_DIR})
