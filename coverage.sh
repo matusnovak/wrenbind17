928abb332e9755691b29bd698f5899c3ac6c66f5
Matus Novak <email@matusnovak.com>
2021-05-25 23:17:29 +0200

    Fix coverage tests remove

--- coverage.sh
+++ coverage.sh
@@ -3,5 +3,5 @@
 set -e
 
 lcov --directory ./build --capture --output-file ./build/coverage.info
-lcov --remove ./build/coverage.info '/usr/*' "${HOME}"'/.cache/*' '*/catch2/*' 'tests/*' --output-file ./build/coverage.info
+lcov --remove ./build/coverage.info '/usr/*' "${HOME}"'/.cache/*' '*/catch2/*' '*/tests/*' --output-file ./build/coverage.info
 lcov --list ./build/coverage.info
